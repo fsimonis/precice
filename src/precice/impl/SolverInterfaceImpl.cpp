5a94706b2c4dfaa8d06c7093b9f30a23922e4556
Frédéric Simonis <simonisfrederic@gmail.com>
2020-10-22 17:28:07 +0200

    Add reinitialize to coupling schemes

--- src/precice/impl/SolverInterfaceImpl.cpp
+++ src/precice/impl/SolverInterfaceImpl.cpp
@@ -290,12 +290,17 @@ double SolverInterfaceImpl::initialize()
     watchPoint->initialize();
   }
 
-  // Initialize coupling state, overwrite these values for restart
-  double time       = 0.0;
-  int    timeWindow = 1;
+  if (_couplingScheme->isInitialized()) {
+    PRECICE_DEBUG("Reinitialize coupling schemes");
+    _couplingScheme->reinitialize();
+  } else {
+    // Initialize coupling state, overwrite these values for restart
+    double time       = 0.0;
+    int    timeWindow = 1;
 
-  PRECICE_DEBUG("Initialize coupling schemes");
-  _couplingScheme->initialize(time, timeWindow);
+    PRECICE_DEBUG("Initialize coupling schemes");
+    _couplingScheme->initialize(time, timeWindow);
+  }
   PRECICE_ASSERT(_couplingScheme->isInitialized());
 
   double dt = 0.0;
